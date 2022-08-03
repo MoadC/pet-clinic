bcd9ca9231b50a6dc8b4e880e07a3aa925fc8252
MoadC <rappelzkloyster@gmail.com>
2022-08-03 18:00:41 +0100

    Adding Interfaces for services

---
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package guru.spring.petclinic.services;
+
+import guru.spring.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package guru.spring.petclinic.services;
+
+import guru.spring.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
