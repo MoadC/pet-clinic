1dc7a9b7fbde43d5fa04616765f2c98ec5bcd519
MoadC <rappelzkloyster@gmail.com>
2022-08-05 07:51:20 +0100

    fix Vets List Page

--- pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/VetController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class VetController {
 
     @RequestMapping({"/vets", "/vets/index"})
-    public String VetsList(){
+    public String listVets(){
 
         return "vets/index";
     }
