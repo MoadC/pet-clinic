9f68be3338033845e4d0673cc7fda9e3a1f91aa8
MoadC <rappelzkloyster@gmail.com>
2022-08-06 13:20:35 +0100

    Showing List of Vets on Vet Index Page

--- pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package guru.spring.petclinic.controllers;
 
+import guru.spring.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
