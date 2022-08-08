662a8d4cd7dcd5fa4881bc488825b5037d701d6e
MoadC <rappelzkloyster@gmail.com>
2022-08-08 02:33:38 +0100

    Fixed Links And added not implemented yet page

--- pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/OwnerController.java
@@ -22,4 +22,10 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping({"/owners/find","/oups" })
+    public String findOwners(){
+        return "notimplemented";
+    }
+
 }
--- pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/VetController.java
@@ -15,7 +15,7 @@ public class VetController {
     }
 
 
-    @RequestMapping({"/vets", "/vets/index"})
+    @RequestMapping({"/vets", "/vets/index", "/vets.html"})
     public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
