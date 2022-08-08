a0688a4eb033b2b3a482adffcb85c3075fcbe92c
MoadC <rappelzkloyster@gmail.com>
2022-08-08 21:46:44 +0100

    Added JPA Mapping to Specialty and Vet entities

--- pet-clinic-data/src/main/java/guru/spring/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/model/Speciality.java
@@ -1,6 +1,14 @@
 package guru.spring.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
