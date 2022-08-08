4c245d05477f64776bc5c2425d5884350c1c9dba
MoadC <rappelzkloyster@gmail.com>
2022-08-08 01:33:32 +0100

    Added Pets and Contact info to owners on startup

--- pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.spring.petclinic.bootstrap;
 
 import guru.spring.petclinic.model.Owner;
+import guru.spring.petclinic.model.Pet;
 import guru.spring.petclinic.model.PetType;
 import guru.spring.petclinic.model.Vet;
 import guru.spring.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.spring.petclinic.services.PetTypeService;
 import guru.spring.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Jake");
         owner1.setLastName("Jackson");
+        owner1.setAddress("123 Street");
+        owner1.setCity("Miami");
+        owner1.setTelephone("111111111");
+
+        Pet jakepet = new Pet();
+        jakepet.setPetType(savedDogPetType);
+        jakepet.setOwner(owner1);
+        jakepet.setBirthDate(LocalDate.now());
+        jakepet.setName("Rosco");
+        owner1.getPets().add(jakepet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Maria");
         owner2.setLastName("Barbara");
+        owner2.setAddress("123 Street");
+        owner2.setCity("Miami");
+        owner2.setTelephone("222222222");
+
+        Pet mariasCat = new Pet();
+        mariasCat.setName("Kitty");
+        mariasCat.setOwner(owner2);
+        mariasCat.setBirthDate(LocalDate.now());
+        mariasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(mariasCat);
 
         ownerService.save(owner2);
 
