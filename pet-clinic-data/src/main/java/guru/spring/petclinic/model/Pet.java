3e4778198fec10295633d76fc95a73f6166e4256
MoadC <rappelzkloyster@gmail.com>
2022-08-08 23:59:46 +0100

    Added Spring Data JPA service for Owners

--- pet-clinic-data/src/main/java/guru/spring/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/model/Pet.java
@@ -23,7 +23,7 @@ public class Pet extends BaseEntity{
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pets")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
