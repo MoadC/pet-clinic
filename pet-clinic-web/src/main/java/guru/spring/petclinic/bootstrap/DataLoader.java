e5ff2c87aca5b9f7145bbee424912eccc8511d84
MoadC <rappelzkloyster@gmail.com>
2022-08-09 00:59:21 +0100

    Added Spring Data JPA Service for Visit

--- pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package guru.spring.petclinic.bootstrap;
 
 import guru.spring.petclinic.model.*;
-import guru.spring.petclinic.services.OwnerService;
-import guru.spring.petclinic.services.PetTypeService;
-import guru.spring.petclinic.services.SpecialtyService;
-import guru.spring.petclinic.services.VetService;
+import guru.spring.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -88,6 +88,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(mariasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(mariasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("active kitty !");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners..");
 
