9c36813bc33442c3e3fc273916047c1e61503e3e
MoadC <rappelzkloyster@gmail.com>
2022-08-09 07:14:15 +0100

    Added Project Lombok.

--- pet-clinic-data/src/main/java/guru/spring/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package guru.spring.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -13,11 +20,4 @@ public class Vet extends Person{
             inverseJoinColumns = @JoinColumn(name="specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
