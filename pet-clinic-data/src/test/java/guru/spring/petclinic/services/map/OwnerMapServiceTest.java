933bbe059a862f8fb453de881c764df02397e8f7
MoadC <rappelzkloyster@gmail.com>
2022-08-11 22:52:11 +0100

    Added Few JUnit5 tests

---
+++ pet-clinic-data/src/test/java/guru/spring/petclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,60 @@
+package guru.spring.petclinic.services.map;
+
+import guru.spring.petclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+
+    final Long ownerId = 1L;
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+
+        ownerMapService.save(Owner.builder().id(ownerId).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerMapService.findAll();
+
+        assertEquals(1,ownerSet.size());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(ownerId);
+
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+
+        Owner owner2 = Owner.builder().id(id).build();
+
+        Owner savedOwner= ownerMapService.save(owner2);
+
+        assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
