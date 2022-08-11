2863af5b09baa7543fc06ba79d30a5f312ba5203
MoadC <rappelzkloyster@gmail.com>
2022-08-11 22:51:41 +0100

    Added More refactoring to Project Lombok Builder.

--- pet-clinic-data/src/main/java/guru/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/model/Owner.java
@@ -1,6 +1,9 @@
 package guru.spring.petclinic.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,11 +12,19 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address,
+                 String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 
     @Column(name = "adress")
     private String address;
