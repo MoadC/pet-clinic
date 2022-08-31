a6068ab8050f4c7599f3cea00be10b7e320eb50b
MoadC <rappelzkloyster@gmail.com>
2022-08-31 08:15:28 +0100

    Updated Owner Controller

--- pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package guru.spring.petclinic.controllers;
 import guru.spring.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OwnerController {
@@ -26,6 +29,13 @@ public class OwnerController {
     @RequestMapping({"/owners/find","/oups" })
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 
 }
